4c6d82baa9951671a2a039e8753ea4373ef3fe70
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-15 14:28:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  } else {
+    
+  }
 }
 
 function total() {
