7a4c6b412e83286536d9fa3d53cfc43e36475392
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-15 15:09:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,8 +69,9 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`
   } else {
-    var order = cart.splice(0)
-    return `Your total cost is ${total(order)}, which will be charged to the card ${cardNumber}.`
+    var placeOrderStr = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    cart.splice(0)
+    return 
   }
   
 }
