b69b07e1b91b21f975c5b2d038863a8adac07a3a
Nalongsak Luangkhot <loongluangkhot@gmail.com>
2018-07-15 15:10:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function placeOrder(cardNumber) {
   if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`
   } else {
-    var placeOrderStr = `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    var placeOrderStr = `Your total cost is \$${total()}, which will be charged to the card ${cardNumber}.`
     cart.splice(0)
     return placeOrderStr
   }
